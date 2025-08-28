ee9df20b289e346925cb207a86bb7eed7125f5fb
hannahhaering <hannah.haering96@gmail.com>
2025-08-28 00:07:54 -0400

    specified log message to view or instrument name

--- src/OpenTelemetry/Metrics/Reader/MetricReaderExt.cs
+++ src/OpenTelemetry/Metrics/Reader/MetricReaderExt.cs
@@ -128,11 +128,22 @@ public abstract partial class MetricReader
 
                 if (!Guard.IsValidInstrumentName(metricStreamIdentity.InstrumentName))
                 {
-                    OpenTelemetrySdkEventSource.Log.MetricInstrumentIgnored(
+                    if (metricStreamConfig?.Name == null)
+                    {
+                        OpenTelemetrySdkEventSource.Log.MetricInstrumentIgnored(
                         metricStreamIdentity.InstrumentName,
                         metricStreamIdentity.MeterName,
-                        "Metric name is invalid.",
+                        "Instrument name is invalid.",
                         "The name must comply with the OpenTelemetry specification.");
+                    }
+                    else
+                    {
+                        OpenTelemetrySdkEventSource.Log.MetricInstrumentIgnored(
+                        metricStreamIdentity.InstrumentName,
+                        metricStreamIdentity.MeterName,
+                        "View name is invalid.",
+                        "The name must comply with the OpenTelemetry specification.");
+                    }
 
                     continue;
                 }
